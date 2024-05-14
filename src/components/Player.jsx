1e0751d8dc6b1e7598038afbe9a11926f30d3168
Talha-Rasool <talharasool450@gmail.com>
2024-05-14 10:05:23 +0200

    Uodated  the state for Input values.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ IntialName, symbol }) {
+  const[playerName,setPlayerName]=useState(IntialName)
   const [isEditing, setIsEditing] = useState(false);
+
+
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  };
+
+
   function handleClick() {
     setIsEditing((editing)=>!editing);
   };
 
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-      playerName = <input type="text" value={name}/>;
+      editablePlayerName = <input type="text" value={playerName} onChange={handleChange}/>;
     }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleClick}>{isEditing?'Save':'Edit'}</button>
