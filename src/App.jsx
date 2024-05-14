c81c8b325bac2d598faa3cc985799f7a9549f5a0
Talha-Rasool <talharasool450@gmail.com>
2024-05-14 10:32:21 +0200

    -Add Player component to manage player information - Update App component to integrate Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+        <Player name='Player 1' symbol='X' />
+        <Player name='Player 2' symbol='Y' />
+          
+          
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
