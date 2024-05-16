02988943087434185bba47267c0d65a1e55a2f4d
Talha-Rasool <talharasool450@gmail.com>
2024-05-16 11:59:17 +0200

    deveolped a GameBoard Component.

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+
+const intialGameBoard=[
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+    
+]
+
+
+export default function GameBoard(){
+    const[gameBoard,setGameBoard]=useState(intialGameBoard);
+
+    function handleClick(indexRow,playerI){
+        setGameBoard((prevGameBoard)=>{
+            const updatedBoard=[...prevGameBoard.map(innerArr=> [...innerArr])];
+            updatedBoard[indexRow][playerI]='X'
+            return updatedBoard;
+
+        })
+        
+    }
+
+
+
+    return <ol id="game-board">
+        {gameBoard.map((row,indexRow)=> <li key={indexRow}>
+            <ol>
+                {row.map((playerN,playerI)=> <li key={playerI}>
+                    <button onClick={()=>handleClick(indexRow,playerI)}>{playerN}</button>
+                </li>)}
+            </ol>
+        </li>)}
+    </ol>
+}
