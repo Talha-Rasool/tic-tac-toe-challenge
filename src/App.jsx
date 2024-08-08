12cf5bc63b3daabbdc7941baa6478ef372e9cf10
Talha-Rasool <talharasool450@gmail.com>
2024-08-08 19:43:30 +0200

    `feat`:added a state to derieved data

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,50 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
-
+import Log from "./components/Log";
 
 export default function App() {
-    const[activePlayer,setActivePlayer]=useState('X');
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-    function handleSelectPlayer(){
-        setActivePlayer((currActive)=>currActive==='X'?'Y':'X');
-       
-    }
+  function handleSelectPlayer(rowInd, colInd) {
+    setActivePlayer((currActive) => (currActive === "X" ? "Y" : "X"));
+    setGameTurns((prevTurn) => {
+      let currentPlayer = "X";
+      if (prevTurn.length > 0 && prevTurn[0].player === "X") {
+        currentPlayer = "Y";
+      }
+      const updatedTurns = [
+        { square: { row: rowInd, col: colInd }, player: currentPlayer },
+        ...prevTurn,
+      ];
+     
+      return updatedTurns;
+    });
+  }
   return (
     <main>
       <Header />
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player IntialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
-          <Player IntialName="Player 2" symbol="Y" isActive={activePlayer==='Y'} />
+          <Player
+            IntialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            IntialName="Player 2"
+            symbol="Y"
+            isActive={activePlayer === "Y"}
+          />
         </ol>
-        <GameBoard onSelectPlayer={handleSelectPlayer} onActive={activePlayer}/>
+        <GameBoard
+          onSelectPlayer={handleSelectPlayer}
+          onActive={activePlayer}
+          turns={gameTurns}
+        />
       </div>
+      <Log turns={gameTurns}/>
       LOG
     </main>
   );
